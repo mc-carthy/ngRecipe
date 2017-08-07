f21ce147792174c7681585cb17e829b88dd9d70d
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-07 18:07:35 +0100

    Create and display sample recipe

--- src/app/recipes/recipe-list/recipe-list.component.ts
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -8,7 +8,13 @@ import { Recipe } from './../recipe.model';
 })
 export class RecipeListComponent implements OnInit {
 
-    recipes: Recipe[] = [];
+    recipes: Recipe[] = [
+        new Recipe(
+            'Test recipe', 
+            'This is a test recipe', 
+            'https://cdn.pixabay.com/photo/2016/06/15/19/09/food-1459693_960_720.jpg'
+        )
+    ];
 
     constructor() { }
 
