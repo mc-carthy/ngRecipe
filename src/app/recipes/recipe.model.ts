e04863d813b2ff2c339be0d9b554495a6af92ac8
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-08 07:03:17 +0100

    Extract single recipe list data into recipe-item and pass data to it from recipe-list

--- src/app/recipes/recipe.model.ts
+++ src/app/recipes/recipe.model.ts
@@ -1,3 +1,3 @@
 export class Recipe {
-    constructor(public name: string, public desc: string, public imagePath: string) { }
+    constructor(public name: string, public description: string, public imagePath: string) { }
 }
