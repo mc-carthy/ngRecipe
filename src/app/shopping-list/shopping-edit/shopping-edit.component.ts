57062f1a4b933130bbdcef4ddf1de22b60b232d5
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-08-11 01:11:26 +0100

    Refactor shopping list with services

--- src/app/shopping-list/shopping-edit/shopping-edit.component.ts
+++ src/app/shopping-list/shopping-edit/shopping-edit.component.ts
@@ -1,5 +1,6 @@
-import { Component, OnInit, ElementRef, ViewChild, EventEmitter, Output } from '@angular/core';
+import { Component, OnInit, ElementRef, ViewChild } from '@angular/core';
 import { Ingredient } from './../../shared/ingredient.model';
+import { ShoppingListService } from './../shopping-list.service';
 
 @Component({
     selector: 'app-shopping-edit',
@@ -11,9 +12,7 @@ export class ShoppingEditComponent implements OnInit {
     @ViewChild('nameInput') nameInputRef: ElementRef;
     @ViewChild('amountInput') amountInputRef: ElementRef;
 
-    @Output() ingredientAdded = new EventEmitter<Ingredient>();
-
-    constructor() { }
+    constructor(private shoppingListService: ShoppingListService) { }
 
     ngOnInit() {
     }
@@ -24,7 +23,7 @@ export class ShoppingEditComponent implements OnInit {
 
         const newIngredient = new Ingredient(name, amount);
 
-        this.ingredientAdded.emit(newIngredient);
+        this.shoppingListService.addIngredient(newIngredient);
     }
 
 }
--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -1,8 +1,31 @@
-import { Injectable } from '@angular/core';
+import { Injectable, EventEmitter } from '@angular/core';
+import { Ingredient } from './../shared/ingredient.model';
 
 @Injectable()
 export class ShoppingListService {
 
-  constructor() { }
+    ingredientsChanged = new EventEmitter<Ingredient[]>();
+
+    private ingredients: Ingredient[] = [
+        new Ingredient(
+            'Apples',
+            2
+        ),
+        new Ingredient(
+            'Tomatoes',
+            10
+        ),
+    ];
+
+    constructor() { }
+
+    getIngredients(): Ingredient[] {
+        return this.ingredients.slice();
+    }
+
+    addIngredient(ingredient: Ingredient) {
+        this.ingredients.push(ingredient);
+        this.ingredientsChanged.emit(this.ingredients.slice());
+    }
 
 }
