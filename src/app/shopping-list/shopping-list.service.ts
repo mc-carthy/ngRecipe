7818328c673f3d1fb7cefeb79fbd8ab551b5f8fa
Mick McCarthy <mccarthy.mick0012@gmail.com>
2017-09-01 11:20:07 +0100

    Add ability to edit existing items

--- src/app/shopping-list/shopping-list.service.ts
+++ src/app/shopping-list/shopping-list.service.ts
@@ -46,6 +46,11 @@ export class ShoppingListService {
         this.ingredientsChanged.next(this.ingredients.slice());
     }
 
+    updateIngredient(index: number, newIngredient: Ingredient) {
+        this.ingredients[index] = newIngredient;
+        this.ingredientsChanged.next(this.ingredients.slice());
+    }
+
     private containsName(ingredients: Ingredient[], ingredient: Ingredient)
     {
         for (let i of ingredients) {
